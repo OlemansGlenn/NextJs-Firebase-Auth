8802d0d32a780806805b5f376de841d0f0113ed5
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-10-18 11:27:36 +0200

    added styling and made navbar toggle with hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,28 +1,47 @@
-import React from "react";
-import Link from "next/link";
-import { useAuth } from "../context/AuthContext";
-import router from "next/router";
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import Link from 'next/link'
+import { useAuth } from '../context/AuthContext'
+import { useRouter } from 'next/router'
 
-const Navbar = () =>{
-    const { user, logout } = useAuth()
-return(
-    <nav>
-        {user? (
-            <div>
-                <button onClick={() => {
+const Navbarcomp = () => {
+  const { user, logout } = useAuth()
+  const router = useRouter()
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Link href="/" passHref>
+          <Navbar.Brand>NextJS Firebase Auth</Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {user ? (
+              <div>
+                <Nav.Link
+                  onClick={() => {
                     logout()
                     router.push('/login')
-                  }}>Logout</button>
-            </div>
-        ):
-        <>
-        <Link href="/login">Login</Link>
-        <Link href="/signup">Register</Link>
-        </>
-        }
-
-    </nav>
-)
+                  }}
+                >
+                  Logout
+                </Nav.Link>
+              </div>
+            ) : (
+              <>
+                <Link href="/signup" passHref>
+                  <Nav.Link>Signup</Nav.Link>
+                </Link>
+                <Link href="/login" passHref>
+                  <Nav.Link>Login</Nav.Link>
+                </Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
-
-export default Navbar
+export default Navbarcomp
