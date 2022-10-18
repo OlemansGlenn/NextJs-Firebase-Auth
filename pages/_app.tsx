fc5ccb68f1306ada46c3909171dbb8500742fe47
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-10-18 10:35:50 +0200

    added authcontextprovider to app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
+import { AuthContextProvider } from '../context/AuthContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
-    <>
+    <AuthContextProvider>
     <Navbar />
     <Component {...pageProps} />
-    </> 
+    </AuthContextProvider> 
   )
  
 }
