6ecbde1be462eb12aedeb7fabf9d0d5612ac9dab
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-10-18 11:13:43 +0200

    changed layout depending on user being logged in or not

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useAuth } from '../context/AuthContext'
 
 const dashboard = () =>{
-    return <div>This is a protected route</div>
+    const { user } = useAuth()
+
+    return (
+        <>
+            {user? (
+                <div>
+                    <h1>Welcome to your</h1>
+                </div>
+              ):<>
+               <div>This is a protected route</div>
+               </>}
+         </>)
 }
 
 export default dashboard
