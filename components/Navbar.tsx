2218f07d4be60998128457bb320046f72495be97
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-10-18 11:14:41 +0200

    added logout button when user is logged in

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import Link from "next/link";
+import { useAuth } from "../context/AuthContext";
+import router from "next/router";
 
 const Navbar = () =>{
+    const { user, logout } = useAuth()
 return(
     <nav>
+        {user? (
+            <div>
+                <button onClick={() => {
+                    logout()
+                    router.push('/login')
+                  }}>Logout</button>
+            </div>
+        ):
+        <>
         <Link href="/login">Login</Link>
         <Link href="/signup">Register</Link>
+        </>
+        }
+
     </nav>
 )
 }
